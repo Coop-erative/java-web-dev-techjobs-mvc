bbdd88634a957b74060e89b2e647795955b6f0d5
coop-eration <c.double.o.per@gmial.com>
2020-02-17 09:45:15 -0600

    Updated searchController line 39 to show search type before colon

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -6,9 +6,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 import static org.launchcode.javawebdevtechjobsmvc.controllers.ListController.columnChoices;
+import static org.launchcode.javawebdevtechjobsmvc.models.JobData.findAll;
 
 /**
  * Created by LaunchCode
@@ -25,4 +27,21 @@ public class SearchController {
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
 
+    @RequestMapping(value = "results")
+    public String processSearchAndUpdateView(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+        ArrayList<Job> jobs;
+
+        if (searchTerm.toLowerCase().equals("all")|| searchTerm.toLowerCase().equals("")){
+            jobs = JobData.findAll();
+            model.addAttribute("title", "All Jobs");
+        }else {
+            jobs = JobData.findByColumnAndValue(searchTerm, searchType);
+            model.addAttribute("title", "Jobs with " + columnChoices.get(searchType) + ": " + searchTerm);
+        }
+
+        model.addAttribute("job", jobs);
+
+        return "search";
+    }
 }
+
